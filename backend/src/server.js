30212aa056185f7135cf67a4a89d8560e09f4a52
ShreyasManake9 <shreyasmanake593@gmail.com>
2026-07-06 22:44:19 +0530

    Added new feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/message", messageRoutes);
 
 // make ready for deployment
-if(process.env.MODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
      app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
      app.get("*", (_,res) => {
